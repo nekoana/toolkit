0d4b2cd2fe3bf88977d6c309ca1f7cf7d508c77f
kouqurong <koqurong@outlook.com>
2024-04-19 22:00:36 +0800

    调整列数

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Menus from "@/app/menus";
 
 export default function Home() {
   return (
-    <main className="container h-screen pb-16 mx-auto p-4 overflow-y-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-9 justify-items-center auto-rows-[160px]">
+    <main className="container h-screen pb-16 mx-auto p-4 overflow-y-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 justify-items-center auto-rows-[160px]">
       <Menus />
     </main>
   );
